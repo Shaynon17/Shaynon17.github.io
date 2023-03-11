3da6d13ea6a112bc4a45b0d888b88a09645206a4
Shannon Miller <shannon.wlmiller@yahoo.com>
2023-03-11 16:42:13 -0600

    finished rummy 500 scoreboard

--- scripts/rummy500.js
+++ scripts/rummy500.js
@@ -1,36 +1,350 @@
-let count = 0
+let player1Count = 0
+
+
 
 document.getElementById("rummy500_minusTenBtn").onclick = function () {
-    count -= 10;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count -= 10;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
 document.getElementById("rummy500_minusFiveBtn").onclick = function () {
-    count -= 5;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count -= 5;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
 document.getElementById("rummy500_minusOneBtn").onclick = function () {
-    count -= 1;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count -= 1;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
 document.getElementById("rummy500_addOneBtn").onclick = function () {
-    count += 1;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count += 1;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
 document.getElementById("rummy500_addFiveBtn").onclick = function () {
-    count += 5;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count += 5;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
 document.getElementById("rummy500_addTenBtn").onclick = function () {
-    count += 10;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count += 10;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
 }
 
+// document.getElementById("rummy500_resetBtn").onclick = function () {
+//     player1Count = 0;
+//     document.getElementById("rummy500_countLabel").innerHTML = player1Count;
+// }
+
+
 document.getElementById("rummy500_resetBtn").onclick = function () {
-    count = 0;
-    document.getElementById("rummy500_countLabel").innerHTML = count;
+    player1Count = 0;
+    player2Count = 0;
+    player3Count = 0;
+    player4Count = 0;
+    player5Count = 0;
+    player6Count = 0;
+    player7Count = 0;
+    player8Count = 0;
+    document.getElementById("rummy500_countLabel").innerHTML = player1Count;
+    document.getElementById("rummy500_countLabel2").innerHTML = player2Count;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count;
+
 }
+
+/* below is for #2 */
+
+
+let player2Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn2").onclick = function () {
+   player2Count-= 10;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+document.getElementById("rummy500_minusFiveBtn2").onclick = function () {
+   player2Count-= 5;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+document.getElementById("rummy500_minusOneBtn2").onclick = function () {
+   player2Count-= 1;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+document.getElementById("rummy500_addOneBtn2").onclick = function () {
+   player2Count+= 1;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+document.getElementById("rummy500_addFiveBtn2").onclick = function () {
+   player2Count+= 5;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+document.getElementById("rummy500_addTenBtn2").onclick = function () {
+   player2Count+= 10;
+    document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+}
+
+// document.getElementById("rummy500_resetBtn2").onclick = function () {
+//    player2Count= 0;
+//     document.getElementById("rummy500_countLabel2").innerHTML =player2Count
+// }
+
+/* below is for #3 */
+
+
+let player3Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn3").onclick = function () {
+    player3Count -= 10;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+document.getElementById("rummy500_minusFiveBtn3").onclick = function () {
+    player3Count -= 5;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+document.getElementById("rummy500_minusOneBtn3").onclick = function () {
+    player3Count -= 1;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+document.getElementById("rummy500_addOneBtn3").onclick = function () {
+    player3Count += 1;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+document.getElementById("rummy500_addFiveBtn3").onclick = function () {
+    player3Count += 5;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+document.getElementById("rummy500_addTenBtn3").onclick = function () {
+    player3Count += 10;
+    document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+}
+
+// document.getElementById("rummy500_resetBtn3").onclick = function () {
+//     player3Count = 0;
+//     document.getElementById("rummy500_countLabel3").innerHTML = player3Count
+// }
+
+
+
+/* Below is # 4 */
+
+
+let player4Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn4").onclick = function () {
+    player4Count -= 10;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+document.getElementById("rummy500_minusFiveBtn4").onclick = function () {
+    player4Count -= 5;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+document.getElementById("rummy500_minusOneBtn4").onclick = function () {
+    player4Count -= 1;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+document.getElementById("rummy500_addOneBtn4").onclick = function () {
+    player4Count += 1;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+document.getElementById("rummy500_addFiveBtn4").onclick = function () {
+    player4Count += 5;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+document.getElementById("rummy500_addTenBtn4").onclick = function () {
+    player4Count += 10;
+    document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+}
+
+// document.getElementById("rummy500_resetBtn4").onclick = function () {
+//     player4Count = 0;
+//     document.getElementById("rummy500_countLabel4").innerHTML = player4Count
+// }
+
+
+/* Below is 5 */
+
+
+let player5Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn5").onclick = function () {
+    player5Count -= 10;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+document.getElementById("rummy500_minusFiveBtn5").onclick = function () {
+    player5Count -= 5;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+document.getElementById("rummy500_minusOneBtn5").onclick = function () {
+    player5Count -= 1;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+document.getElementById("rummy500_addOneBtn5").onclick = function () {
+    player5Count += 1;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+document.getElementById("rummy500_addFiveBtn5").onclick = function () {
+    player5Count += 5;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+document.getElementById("rummy500_addTenBtn5").onclick = function () {
+    player5Count += 10;
+    document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+}
+
+// document.getElementById("rummy500_resetBtn5").onclick = function () {
+//     player5Count = 0;
+//     document.getElementById("rummy500_countLabel5").innerHTML = player5Count
+// }
+
+
+/* Below is 6 */
+
+
+let player6Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn6").onclick = function () {
+    player6Count -= 10;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+document.getElementById("rummy500_minusFiveBtn6").onclick = function () {
+    player6Count -= 5;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+document.getElementById("rummy500_minusOneBtn6").onclick = function () {
+    player6Count -= 1;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+document.getElementById("rummy500_addOneBtn6").onclick = function () {
+    player6Count += 1;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+document.getElementById("rummy500_addFiveBtn6").onclick = function () {
+    player6Count += 5;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+document.getElementById("rummy500_addTenBtn6").onclick = function () {
+    player6Count += 10;
+    document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+}
+
+// document.getElementById("rummy500_resetBtn6").onclick = function () {
+//     player6Count = 0;
+//     document.getElementById("rummy500_countLabel6").innerHTML = player6Count
+// }
+
+/* Below is 7 */
+
+
+let player7Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn7").onclick = function () {
+    player7Count -= 10;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+document.getElementById("rummy500_minusFiveBtn7").onclick = function () {
+    player7Count -= 5;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+document.getElementById("rummy500_minusOneBtn7").onclick = function () {
+    player7Count -= 1;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+document.getElementById("rummy500_addOneBtn7").onclick = function () {
+    player7Count += 1;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+document.getElementById("rummy500_addFiveBtn7").onclick = function () {
+    player7Count += 5;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+document.getElementById("rummy500_addTenBtn7").onclick = function () {
+    player7Count += 10;
+    document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+}
+
+// document.getElementById("rummy500_resetBtn7").onclick = function () {
+//     player7Count = 0;
+//     document.getElementById("rummy500_countLabel7").innerHTML = player7Count
+// }
+
+/* Below is 8 */
+
+
+let player8Count = 0
+
+
+document.getElementById("rummy500_minusTenBtn8").onclick = function () {
+    player8Count -= 10;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+document.getElementById("rummy500_minusFiveBtn8").onclick = function () {
+    player8Count -= 5;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+document.getElementById("rummy500_minusOneBtn8").onclick = function () {
+    player8Count -= 1;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+document.getElementById("rummy500_addOneBtn8").onclick = function () {
+    player8Count += 1;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+document.getElementById("rummy500_addFiveBtn8").onclick = function () {
+    player8Count += 5;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+document.getElementById("rummy500_addTenBtn8").onclick = function () {
+    player8Count += 10;
+    document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+}
+
+// document.getElementById("rummy500_resetBtn8").onclick = function () {
+//     player8Count = 0;
+//     document.getElementById("rummy500_countLabel8").innerHTML = player8Count
+// }
