0d22d814ccd2f030cf256e14a4ed60805ce0980d
Shannon Miller <shannon.wlmiller@yahoo.com>
2023-07-22 18:01:57 -0600

    updated scoreboard on ninetyNine

--- scripts/ninetyNine.js
+++ scripts/ninetyNine.js
@@ -19,6 +19,15 @@ document.getElementById("ninetyNine_increaseBtn").onclick = function () {
     }
 }
 
+document.getElementById("ninetyNine_increaseTenBtn").onclick = function () {
+    count += 10;
+    if (count <= 99) {
+        return document.getElementById("ninetyNine_countLabel").innerHTML = count;
+    } else {
+        return document.getElementById("ninetyNine_countLabel").innerHTML = count = 0;
+    }
+}
+
 document.getElementById("ninetyNine_kingBtn").onclick = function () {
     count = 99;
     document.getElementById("ninetyNine_countLabel").innerHTML = count;
