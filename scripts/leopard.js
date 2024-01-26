f8a2f14cf5798567a5c26aacb58a019ff6790161
Shannon Miller <shannon.wlmiller@yahoo.com>
2024-01-26 00:10:06 -0600

    added scoreboard to leopard

---
+++ scripts/leopard.js
@@ -0,0 +1,31 @@
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+function player1PlusOne() {
+    playerOneScore += 1;
+    document.getElementById("player-one-score").innerHTML = playerOneScore;
+}
+
+function player1MinusOne() {
+    playerOneScore -= 1;
+    document.getElementById("player-one-score").innerHTML = playerOneScore;
+
+}
+
+function player2PlusOne() {
+    playerTwoScore += 1;
+    document.getElementById("player-two-score").innerHTML = playerTwoScore;
+}
+
+function player2MinusOne() {
+    playerTwoScore -= 1;
+    document.getElementById("player-two-score").innerHTML = playerTwoScore;
+}
+
+
+function resetScore() {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    document.getElementById("player-one-score").innerHTML = playerOneScore;
+    document.getElementById("player-two-score").innerHTML = playerTwoScore;
+}
